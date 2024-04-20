80cd38422cb0cf244ad247f7be3fc276d145dd27
Julieta Loizzo <juliettloizzo@gmail.com>
2024-04-20 19:56:43 -0300

    Desafio2

---
+++ main.js
@@ -0,0 +1,5 @@
+let nombreProducto = "Macetas" ;
+let precioUnitario = 1200 ;
+let cantidadDeseada = prompt('¿Cuántos productos quisieras comprar?');
+let precioTotal = precioUnitario * cantidadDeseada ;
+alert("El total de su compra de " +nombreProducto + " es de: " + precioTotal) ;
