9523d4682a578a2e65aa91143862c4525cca517a
JulietaLoizzo <166879771+JulietaLoizzo@users.noreply.github.com>
2024-05-03 20:32:51 -0300

    MAIN

--- main.js
+++ main.js
@@ -1,17 +1,5 @@
-const nombreProducto = "Macetas";
-const precioUnitario = 1200; // Cambiado a un número
-
-let cantidadDeseada = prompt(`Ingrese la cantidad de ${nombreProducto} que desea comprar:`);
-cantidadDeseada = parseInt(cantidadDeseada); // Asignando el resultado de parseInt
-
-if (isNaN(cantidadDeseada) || cantidadDeseada <= 0) {
-  alert("Debe ingresar un número válido mayor a cero.");
-} else {
-  let costoTotalSinDescuentos = precioUnitario * cantidadDeseada;
-
-  if (cantidadDeseada >= 5) {
-    costoTotalSinDescuentos *= 0.9; 
-  }
-
-  alert(`El costo total de ${cantidadDeseada} ${nombreProducto} es $${costoTotalSinDescuentos}`);
-}
+let nombreProducto = "Macetas" ;
+let precioUnitario = 1200 ;
+let cantidadDeseada = prompt('¿Cuántos productos quisieras comprar?');
+let precioTotal = precioUnitario * cantidadDeseada ;
+alert("El total de su compra de " +nombreProducto + " es de: " + precioTotal) ;
